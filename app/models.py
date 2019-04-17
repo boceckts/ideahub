642cb2e312fc53d04bd5130a96085e8bf9e39e88
Noah <noahlester@hotmail.com>
2019-04-17 15:02:28 +0800

    1. Modified the User table to use username instead of nickname
    2. Added Login, Logout and Registration functionality

--- app/models.py
+++ app/models.py
@@ -1,16 +1,25 @@
-from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import db, login
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nickname = db.Column(db.String(64), index=True, unique=True)
+    username = db.Column(db.String(64), index=True, unique=True)
     name = db.Column(db.String(64), index=True)
     surname = db.Column(db.String(64), index=True)
     email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
     ideas = db.relationship('Idea', backref='author', lazy='dynamic')
     votes = db.relationship('Vote', backref='owner', lazy='dynamic')
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Idea(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -32,3 +41,7 @@ class Vote(db.Model):
 
     def __repr__(self):
         return '<Vote %r>' % (self.value)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
--- config.py
+++ config.py
@@ -1,9 +1,15 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+# Not sure how we can get around duplicating the line of code below yet...
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-extend_existing=True
 
-DEBUG = True
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    extend_existing=True
+
+    DEBUG = True
