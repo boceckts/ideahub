3008d7fec15838bc9eb7765d02f59cc8c2cb2b78
Tobias Boceck <boceckts@gmail.com>
2019-04-19 20:46:10 +0800

    Removed idea post and put methods from api - you should not be able to create or edit an idea without a user id

--- app/apis/idea_api.py
+++ app/apis/idea_api.py
@@ -1,7 +1,5 @@
-from datetime import datetime
-from flask import request
 from flask_restplus import Resource, Namespace, fields
-from sqlalchemy.exc import IntegrityError
+
 from app import db
 from app.models import Idea
 from app.utils.db_utils import expand_idea, expand_ideas
@@ -33,24 +31,6 @@ class IdeasResource(Resource):
         ideas = Idea.query.all()
         return expand_ideas(ideas), 200
 
-    @idea_ns.expect(new_idea, 201, 'Idea created', validate=True)
-    @idea_ns.response(400, 'Bad request')
-    @idea_ns.response(409, 'Idea already exists')
-    def post(self):
-        """Create a new idea"""
-        json_data = request.get_json(force=True)
-        new_idea = Idea()
-        new_idea.title = json_data['title']
-        new_idea.description = json_data['description']
-        new_idea.categories = json_data['categories']
-        new_idea.tags = json_data['tags']
-        db.session.add(new_idea)
-        try:
-            db.session.commit()
-        except IntegrityError:
-            idea_ns.abort(409, "Idea already exists")
-        return "{}/{}".format(request.url, new_idea.id), 201
-
     @idea_ns.response(204, 'Ideas successfully deleted')
     def delete(self):
         """Delete all ideas"""
@@ -71,27 +51,6 @@ class IdeaResource(Resource):
         if queried_idea is None:
             idea_ns.abort(404, 'Idea not found')
         return expand_idea(queried_idea), 200
-
-    @idea_ns.expect(new_idea, 204, 'Idea was successfully modified', validate=True)
-    @idea_ns.response(409, "Idea already exists")
-    @idea_ns.response(400, 'Bad request')
-    def put(self, idea_id):
-        """Update the idea with the selected idea_id"""
-        if Idea.query.get(idea_id) is None:
-            idea_ns.abort(404, 'Idea not found')
-        json_data = request.get_json(force=True)
-        try:
-            db.session.query(Idea).filter_by(id=idea_id).update({
-                Idea.title: json_data['title'],
-                Idea.description: json_data['description'],
-                Idea.categories: json_data['categories'],
-                Idea.tags: json_data['tags'],
-                Idea.modified: datetime.utcnow()
-            })
-            db.session.commit()
-        except IntegrityError:
-            idea_ns.abort(409, "Idea already exists")
-        return '', 204
 
     @idea_ns.response(204, 'Idea was successfully deleted')
     def delete(self, idea_id):
