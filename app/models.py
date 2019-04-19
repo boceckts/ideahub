18bc6aafa32c88d8f8e6c27363c5ace65cc8c7e7
Tobias Boceck <boceckts@gmail.com>
2019-04-19 14:20:42 +0800

    Adjusted the idea and vote models

--- app/models.py
+++ app/models.py
@@ -35,13 +35,16 @@ class Idea(db.Model):
     attachments = db.Column(db.BLOB)
     categories = db.Column(db.String(64))
     tags = db.Column(db.String(128))
-    created = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    modified = db.Column(db.DateTime)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+    modified = db.Column(db.DateTime, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     votes = db.relationship('Vote', backref='target', lazy='dynamic')
 
     def __repr__(self):
         return '<Idea %r>' % self.title
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
 
 class Vote(db.Model):
@@ -53,6 +56,9 @@ class Vote(db.Model):
 
     def __repr__(self):
         return '<Vote %r>' % self.value
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
 @login.user_loader
 def load_user(id):
