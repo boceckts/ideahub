676fd9b467178ed45d949a52aa615bdbb23cb7e8
Tobias Boceck <boceckts@gmail.com>
2019-05-01 12:43:01 +0800

    Created new profile view and renamed account view to ideas view

--- app/views.py
+++ app/views.py
@@ -1,15 +1,19 @@
+from datetime import datetime
+
 from flask import render_template, flash, redirect, url_for, request
-from flask_login import current_user, login_user, logout_user, login_required
+from flask_login import current_user, login_user, logout_user
 from werkzeug.urls import url_parse
+
+from app import app, db
 from app.forms import LoginForm, RegistrationForm, NewIdeaForm, EditIdeaForm
 from app.models import User, Idea
-from app import app, db
-from datetime import datetime
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html", title='Home')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -28,10 +32,12 @@ def login():
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -47,10 +53,12 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
+
 @app.route('/leaderboard')
 def leaderboard():
     return redirect(url_for('index'))
     # return render_template('leaderboard.html', title='Leaderboard')
+
 
 @app.route('/newIdea', methods=['GET', 'POST'])
 def newIdea():
@@ -59,12 +67,14 @@ def newIdea():
     form = NewIdeaForm()
     if form.validate_on_submit():
         print(current_user.id)
-        idea = Idea(title=form.title.data, description=form.description.data, user_id=current_user.id) #not sure if i have to initialise votes here
+        idea = Idea(title=form.title.data, description=form.description.data,
+                    user_id=current_user.id)  # not sure if i have to initialise votes here
         db.session.add(idea)
         db.session.commit()
         flash('Your idea has been saved!')
         return redirect(url_for('index'))
     return render_template('newIdea.html', title='New Idea', form=form)
+
 
 @app.route('/editIdea/<int:id>', methods=['GET', 'POST'])
 def editIdea(id):
@@ -79,15 +89,23 @@ def editIdea(id):
             idea.modified = datetime.utcnow()
             db.session.commit()
             flash('Your idea has been edited!')
-            return redirect(url_for('account'))
+            return redirect(url_for('ideas'))
         if request.method == 'DELETE':
             flash('Your idea has been deleted! (not working yet)')
-            return redirect(url_for('account'))
+            return redirect(url_for('ideas'))
         return render_template('editIdea.html', title='Edit Idea', form=form, idea=idea)
 
-@app.route('/account', methods=['GET', 'POST'])
-def account():
+
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+    return render_template("profile.html", title='Account')
+
+
+@app.route('/ideas', methods=['GET', 'POST'])
+def ideas():
     if not current_user.is_authenticated:
         return redirect(url_for('login'))
     ideas = Idea.query.filter_by(user_id=current_user.id)
-    return render_template("account.html", title='Account', ideas=ideas)
+    return render_template("ideas.html", title='Ideas', ideas=ideas)
